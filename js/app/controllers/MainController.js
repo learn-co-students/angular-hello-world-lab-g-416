e5228761e5894b3f5a59dfa951734da8dfcbbf61
Jordan Kline <jordan.kline@gmail.com>
2019-04-26 08:59:50 -0700

    Done.

---
+++ js/app/controllers/MainController.js
@@ -0,0 +1,9 @@
+function MainController($scope) {
+    $scope.name = "Paul";
+    $scope.email = "paul@paul.com";
+    $scope.phone = "8675309";
+
+}
+
+
+angular.module('app').controller('MainController', MainController);
