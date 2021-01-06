153ca0ccbebb2ded0f7365eecf84fdb16f748f73
github username <github email address>
2021-01-05 21:24:29 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,1 @@
-angular
-  .module('app', ['ngRoute'])
+angular.module("app",["ngRoute"])
--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -3,9 +3,9 @@ $scope.contact={
     name: "Caitlin",
     email: "testing@gmail.com",
     phone: "2033133919"
-}
+};
 }
 
 var module=angular.module("app")
 
-module.controller("MainController",MainController)
+module.controller("MainController", MainController) 
