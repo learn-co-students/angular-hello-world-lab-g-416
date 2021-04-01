8fab14fde19ccebf921817ee0ed35fdf3657781d
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-04-01 09:07:31 -0400

    Done.

---
+++ js/app/controllers/MainController.js
@@ -0,0 +1,7 @@
+function MainController($scope) {
+  $scope.name = "Bill Gates";
+  $scope.email = "bill@microsoft.com";
+  $scope.phone = "01234567890";
+}
+
+angular.module("app").controller("MainController", MainController);
